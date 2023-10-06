f3967f94aef16bf08bc57a0c316243089b478206
manovarma <144982813+manovarma@users.noreply.github.com>
2023-10-05 19:25:53 -0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,23 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-variable "gcp_project_id" {
-  type        = string
-  description = "The GCP project ID to apply this config to"
-}
 
 variable "name" {
   type        = string
   description = "Name given to the new GKE cluster"
   default     = "online-boutique"
 }
-
-variable "region" {
-  type        = string
-  description = "Region of the new GKE cluster"
-  default     = "us-central1"
-}
-
 variable "namespace" {
   type        = string
   description = "Kubernetes Namespace in which the Online Boutique resources are to be deployed"
